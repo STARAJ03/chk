3a9330e964a1fba3f54d49888edded903c0aa30b
SaurabhJha77 <saurabhjha77777@gmail.com>
2025-05-17 12:07:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config(object):
+    BOT_TOKEN = os.environ.get("7603563090:AAHPy_gegtxcX_BPnyaENGJisrFtVFp7tyE")
+    API_ID = int(os.environ.get("26003553"))
+    API_HASH = os.environ.get("c1f27c622acecf9bf6e71d0d295e75f9")
+    AUTH_USER = os.environ.get('8152589217,6073936818').split(',')
+    AUTH_USERS = [8152589217,6073936818]
+    HOST = "https://drm-api-six.vercel.app"
+    CREDIT = "[SAURABH BOTS]"
